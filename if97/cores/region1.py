239f2e958b0f29e1a660ac785702c119419722bf
Noertri <trinuruns@gmail.com>
2022-04-17 21:01:37 +0700

    memodifikasi modul region 1

--- if97/cores/region1.py
+++ if97/cores/region1.py
@@ -1,108 +1,37 @@
-from if97.koefisien import BIGR, koefReg1
+from if97.koefisien import IJnReg1
 
 
-class __BasicReg1__:
-    """Persamaan2 dasar untuk region 1"""
+def gamma(pi, tau, desc=None):
+    """Fungsi untuk persamaan gamma dan turunannya"""
 
-    def __init__(self):
-        self.capi = koefReg1["capi"]
-        self.capj = koefReg1["capj"]
-        self.n = koefReg1["n"]
+    _gamma = dict()
+    _gamma["gamma"] = 0.
+    _gamma["dgammadpi"] = 0.
+    _gamma["dgammadpi2"] = 0.
+    _gamma["dgammadtau"] = 0.
+    _gamma["dgammadtau2"] = 0.
+    _gamma["dgammadpidtau"] = 0.
 
-    def gamma(self, pi0, tau0):
-        ans = 0.
-        for k in range(34):
-            ans += self.n[k]*((7.1-pi0)**self.capi[k])*((tau0-1.222)**self.capj[k])
+    for Ii, Ji, ni in zip(IJnReg1.values()):
+        _gamma["gamma"] += ni*((7.1-pi)**Ii)*((tau-1.222)**Ji)
+        _gamma["dgammadpi"] += -ni*Ii*((7.1-pi)**(Ii-1))*((tau-1.222)**Ji)
+        _gamma["dgammadpi2"] += ni*Ii*(Ii-1)*((7.1-pi)**(Ii-2))*((tau-1.222)**Ji)
+        _gamma["dgammadtau"] += ni*Ji*((7.1-pi)**Ii)*((tau-1.222)**(Ji-1))
+        _gamma["dgammadtau2"] += ni*Ji*(Ji-1)*((7.1-pi)**Ii)*((tau-1.222)**(Ji-2))
+        _gamma["dgammadpidtau"] += -ni*Ii*Ji*((7.1-pi)**(Ii-1))*((tau-1.222)**(Ji-1))
 
-        return ans
-
-    def parGammaPi(self, pi0, tau0):
-        ans = 0.
-        for k in range(34):
-            ans += -self.n[k]*self.capi[k]*((7.1-pi0)**(self.capi[k]-1))*((tau0-1.222)**self.capj[k])
-
-        return ans
-
-    def parGammaPi2(self, pi0, tau0):
-        ans = 0.
-        for k in range(34):
-            ans += self.n[k]*self.capi[k]*(self.capi[k]-1)*((7.1-pi0)**(self.capi[k]-2))*((tau0-1.222)**self.capj[k])
-
-        return ans
-
-    def parGammaTau(self, pi0, tau0):
-        ans = 0.
-        for k in range(34):
-            ans += self.n[k]*self.capj[k]*((7.1-pi0)**self.capi[k])*((tau0-1.222)**(self.capj[k]-1))
-
-        return ans
-
-    def parGammaTau2(self, pi0, tau0):
-        ans = 0.
-        for k in range(34):
-            ans += self.n[k]*self.capj[k]*(self.capj[k]-1)*((7.1-pi0)**self.capi[k])*((tau0-1.222)**(self.capj[k]-2))
-
-        return ans
-
-    def parGammaPiTau(self, pi0, tau0):
-        ans = 0.
-        for k in range(34):
-            ans += -self.n[k]*self.capi[k]*self.capj[k]*((7.1-pi0)**(self.capi[k]-1))*((tau0-1.222)**(self.capj[k]-1))
-
-        return ans
-
-
-class PropReg1:
-    """Properties2 pada region 1"""
-
-    def __init__(self):
-        self.p0 = 16.53e3
-        self.t0 = 1386
-        self.basic = __BasicReg1__()
-
-    def getSpecVol(self, p, t):
-        pi = p/self.p0
-        tau = self.t0/t
-        gamma1 = self.basic.parGammaPi(pi0=pi, tau0=tau)
-        ans = ((BIGR*t)/p)*pi*gamma1
-        return ans
-
-    def getSpecInternal(self, p, t):
-        pi = p/self.p0
-        tau = self.t0/t
-        gamma1 = self.basic.parGammaTau(pi0=pi, tau0=tau)
-        gamma2 = self.basic.parGammaPi(pi0=pi, tau0=tau)
-        ans = (BIGR*t)*(tau*gamma1-pi*gamma2)
-        return ans
-
-    def getSpecEntropy(self, p, t):
-        pi = p/self.p0
-        tau = self.t0/t
-        gamma1 = self.basic.gamma(pi0=pi, tau0=tau)
-        gamma2 = self.basic.parGammaTau(pi0=pi, tau0=tau)
-        ans = BIGR*(tau*gamma2-gamma1)
-        return ans
-
-    def getSpecEnthalpy(self, p, t):
-        pi = p/self.p0
-        tau = self.t0/t
-        gamma1 = self.basic.parGammaTau(pi0=pi, tau0=tau)
-        ans = BIGR*t*tau*gamma1
-        return ans
-
-    def getCp(self, p, t):
-        pi = p/self.p0
-        tau = self.t0/t
-        gamma1 = self.basic.parGammaTau2(pi0=pi, tau0=tau)
-        ans = -BIGR*(tau**2)*gamma1
-        return ans
-
-    def getCv(self, p, t):
-        pi = p/self.p0
-        tau = self.t0/t
-        gamma1 = self.basic.parGammaPi(pi0=pi, tau0=tau)
-        gamma2 = self.basic.parGammaPi2(pi0=pi, tau0=tau)
-        gamma3 = self.basic.parGammaTau2(pi0=pi, tau0=tau)
-        gamma4 = self.basic.parGammaPiTau(pi0=pi, tau0=tau)
-        ans = -BIGR*(tau**2)*gamma3+BIGR*(((gamma1-tau*gamma4)**2)/gamma2)
-        return ans
+    match desc:
+        case "gamma":
+            return _gamma["gamma"]
+        case "dgammadpi":
+            return _gamma["dgammadpi"]
+        case "dgammadpi2":
+            return _gamma["dgammadpi2"]
+        case "dgammadtau":
+            return _gamma["dgammadtau"]
+        case "dgammadtau2":
+            return _gamma["dgammadtau2"]
+        case "dgammadpidtau":
+            return _gamma["dgammadpidtau"]
+        case _:
+            return Nome
