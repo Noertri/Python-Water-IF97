1b70c4783cad7163c61b9124124d1062e07082af
Noertri <trinuruns@gmail.com>
2022-04-17 20:17:37 +0700

    memodifikasi, tes, dan debug region 4

--- if97/cores/region4.py
+++ if97/cores/region4.py
@@ -3,21 +3,21 @@ import numpy as np
 
 
 def get_satur_press(tsat):
-    _n = nReg4["n"]
-    nu = (tsat/1)+(_n[8]/((tsat/1)-_n[9]))
-    Ai = (nu**2) + _n[0]*nu + _n[1]
-    Bi = _n[2]*(nu**2) + _n[3]*nu + _n[4]
-    Ci = _n[5]*(nu**2) + _n[6]*nu + _n[7]
+    n = nReg4["n"]
+    nu = (tsat/1)+(n[8]/((tsat/1)-n[9]))
+    Ai = (nu**2)+n[0]*nu+n[1]
+    Bi = n[2]*(nu**2)+n[3]*nu+n[4]
+    Ci = n[5]*(nu**2)+n[6]*nu+n[7]
     ans = 1e3*((2*Ci/(-Bi + np.sqrt(Bi**2 - 4*Ai*Ci)))**4)
     return ans
 
 
 def get_satur_temp(psat):
-    _n = nReg4["n"]
+    n = nReg4["n"]
     beta = (psat/1000)**(1/4)
-    Ei = (beta**2) + _n[2]*beta + _n[5]
-    Fi = _n[0]*(beta**2) + _n[3]*beta + _n[6]
-    Gi = _n[1]*(beta**2) + _n[4]*beta + _n[7]
+    Ei = (beta**2)+n[2]*beta+n[5]
+    Fi = n[0]*(beta**2)+n[3]*beta+n[6]
+    Gi = n[1]*(beta**2)+n[4]*beta+n[7]
     Di = 2*Gi/(-Fi - np.sqrt((Fi**2) - 4*Ei*Gi))
-    ans = 1*((_n[9] + Di - np.sqrt((_n[9] + Di)**2 - 4*(_n[8] + _n[9]*Di)))/2)
+    ans = 1*((n[9]+Di-np.sqrt((n[9]+Di)**2-4*(n[8]+n[9]*Di)))/2)
     return ans
