af1952bd400bcefda100a4138b5cd1479efe9784
Noertri <trinuruns@gmail.com>
2022-04-19 21:03:39 +0700

    memodifikasi class Boundary23, tes dan debug class Boundary23

--- if97/_if97.py
+++ if97/_if97.py
@@ -3,6 +3,4 @@ from .cores import region1, region2, region3, region4, region5, boundary
 
 
 def saturation_temp(tsat):
-
-    psat = region4.get_satur_press(tsat)
-
+    pass
--- if97/cores/boundary.py
+++ if97/cores/boundary.py
@@ -1,15 +1,15 @@
 from math import sqrt
-from ..koefisien import b23
+from ..koefisien import nb23
 
 
 class Boundary23:
     """Persamaan garis batas untuk region 2 dan 3"""
 
-    _n = b23
+    _n = nb23
 
     @classmethod
     def getPress(cls, t):
-
+        _n = nb23
         theta = t/1
         ans = cls._n[0] + cls._n[1]*theta + cls._n[2]*(theta**2)
         return ans*1000
@@ -17,5 +17,5 @@ class Boundary23:
     @classmethod
     def getTemp(cls, p):
         pi = p/1000
-        ans = cls._n[3] + sqrt((pi - cls._n[4])/_n[2])
+        ans = cls._n[3] + sqrt((pi - cls._n[4])/cls._n[2])
         return ans*1
