d24287758bde9d0473e0b570a989531a3ad1002f
Noertri <trinuruns@gmail.com>
2022-04-19 20:42:48 +0700

    memodifikasi region3, tes dan debug region3

--- if97/cores/region3.py
+++ if97/cores/region3.py
@@ -4,56 +4,53 @@ from ..koefisien import IJnReg3, RHOC, TEMPC, BIGR
 
 class Region3:
 
-    def __init__(self, rho, t):
-        self.rho = rho
-        self.t = t
-        self.delta = rho/RHOC
-        self.tau = TEMPC/t
-
-    def phi(self, desc):
+    def phi(self, delta, tau, desc):
 
         n = IJnReg3["n"]
         I = IJnReg3["I"]
         J = IJnReg3["J"]
         _phi = dict()
-        _phi["phi"] = n[0]*np.log(self.delta)
-        _phi["dphiddelta"] = n[0]/self.delta
-        _phi["dphiddelta2"] = (-1*n[0])/(self.delta**2)
+        _phi["phi"] = n[0]*np.log(delta)
+        _phi["dphiddelta"] = n[0]/delta
+        _phi["dphiddelta2"] = (-1*n[0])/(delta**2)
         _phi["dphidtau"] = 0.
         _phi["dphidtau2"] = 0.
         _phi["dphiddeltadtau"] = 0.
 
         for Ii, Ji, ni in zip(I[1:], J[1:], n[1:]):
-            _phi["phi"] += ni*(self.delta**Ii)*(self.tau**Ji)
-            _phi["dphiddelta"] += ni*Ii*(self.delta**(Ii-1))*(self.tau**Ji)
-            _phi["dphiddelta2"] += ni*Ii*(Ii-1)*(self.delta**(Ii-2))*(self.tau**Ji)
-            _phi["dphidtau"] += ni*Ji*(self.delta**Ii)*(self.tau**(Ji-1))
-            _phi["dphidtau2"] += ni*Ji*(Ji-1)*(self.delta**Ii)*(self.tau**(Ji-2))
-            _phi["dphiddeltadtau"] += ni*Ii*Ji*(self.delta**(Ii-1))*(self.tau**(Ji-1))
+            _phi["phi"] += ni*(delta**Ii)*(tau**Ji)
+            _phi["dphiddelta"] += ni*Ii*(delta**(Ii-1))*(tau**Ji)
+            _phi["dphiddelta2"] += ni*Ii*(Ii-1)*(delta**(Ii-2))*(tau**Ji)
+            _phi["dphidtau"] += ni*Ji*(delta**Ii)*(tau**(Ji-1))
+            _phi["dphidtau2"] += ni*Ji*(Ji-1)*(delta**Ii)*(tau**(Ji-2))
+            _phi["dphiddeltadtau"] += ni*Ii*Ji*(delta**(Ii-1))*(tau**(Ji-1))
 
         if desc and desc.lower() in _phi.keys():
             return _phi[desc.lower()]
         else:
             return None
 
-    def get_properties(self, desc=None):
+    def get_properties(self, rho, t, desc=None):
 
         props = dict()
 
-        phi = self.phi(desc="phi")
-        dphiddelta = self.phi(desc="dphiddelta")
-        dphiddelta2 = self.phi(desc="dphiddelta2")
-        dphidtau = self.phi(desc="dphidtau")
-        dphidtau2 = self.phi(desc="dphidtau2")
-        dphiddeltadtau = self.phi(desc="dphiddeltadtau")
+        delta = rho/RHOC
+        tau = TEMPC/t
 
-        props["p"] = self.rho*BIGR*self.t*self.delta*dphiddelta
-        props["u"] = BIGR*self.t*self.tau*dphidtau
-        props["s"] = BIGR*(self.tau*dphidtau-phi)
-        props["h"] = BIGR*self.t*(self.tau*dphidtau+self.delta*dphiddelta)
+        phi = self.phi(delta, tau, desc="phi")
+        dphiddelta = self.phi(delta, tau, desc="dphiddelta")
+        dphiddelta2 = self.phi(delta, tau, desc="dphiddelta2")
+        dphidtau = self.phi(delta, tau, desc="dphidtau")
+        dphidtau2 = self.phi(delta, tau, desc="dphidtau2")
+        dphiddeltadtau = self.phi(delta, tau, desc="dphiddeltadtau")
+
+        props["p"] = rho*BIGR*t*delta*dphiddelta
+        props["u"] = BIGR*t*tau*dphidtau
+        props["s"] = BIGR*(tau*dphidtau-phi)
+        props["h"] = BIGR*t*(tau*dphidtau+delta*dphiddelta)
         props["cv"] = -1*BIGR*(dphidtau2**2)
-        props["cp"] = BIGR*(-1*(self.tau**2)*dphidtau2+(((self.delta*dphiddelta-self.delta*self.tau*dphiddeltadtau)**2)/(
-                2*self.delta*dphiddelta+(self.delta**2)*dphiddelta2)))
+        props["cp"] = BIGR*(-1*(tau**2)*dphidtau2+(((delta*dphiddelta-delta*tau*dphiddeltadtau)**2)/(
+                2*delta*dphiddelta+(delta**2)*dphiddelta2)))
 
         if desc and desc.lower() in props.keys():
             return props[desc.lower()]
--- if97/tests/test_region3.py
+++ if97/tests/test_region3.py
@@ -1,13 +1,14 @@
 import unittest
+from IF97 import RHOC, TEMPC
 from IF97.cores import region3
 
 
 class TestRegion3(unittest.TestCase):
 
     def setUp(self):
-        self.props1 = region3.Region3(rho=500, t=650)
-        self.props2 = region3.Region3(rho=200, t=650)
-        self.props3 = region3.Region3(rho=500, t=750)
+        self.props1 = region3.Region3()
+        self.props2 = region3.Region3()
+        self.props3 = region3.Region3()
 
     def test_properties1(self):
         p_des = 0.255837018e2
@@ -16,11 +17,11 @@ class TestRegion3(unittest.TestCase):
         s_des = 0.405427273e1
         cp_des = 0.138935717e2
 
-        self.assertAlmostEqual(self.props1.get_properties("p")*1e-3, p_des, delta=1e-7)
-        self.assertAlmostEqual(self.props1.get_properties("h"), h_des, delta=1e-5)
-        self.assertAlmostEqual(self.props1.get_properties("u"), u_des, delta=1e-5)
-        self.assertAlmostEqual(self.props1.get_properties("s"), s_des, delta=1e-8)
-        self.assertAlmostEqual(self.props1.get_properties("cp"), cp_des, delta=1e-7)
+        self.assertAlmostEqual(self.props1.get_properties(rho=500, t=650, desc="p")*1e-3, p_des, delta=1e-7)
+        self.assertAlmostEqual(self.props1.get_properties(rho=500, t=650, desc="h"), h_des, delta=1e-5)
+        self.assertAlmostEqual(self.props1.get_properties(rho=500, t=650, desc="u"), u_des, delta=1e-5)
+        self.assertAlmostEqual(self.props1.get_properties(rho=500, t=650, desc="s"), s_des, delta=1e-8)
+        self.assertAlmostEqual(self.props1.get_properties(rho=500, t=650, desc="cp"), cp_des, delta=1e-7)
 
     def test_properties2(self):
         p_des = 0.222930643e2
@@ -29,11 +30,11 @@ class TestRegion3(unittest.TestCase):
         s_des = 0.485438792e1
         cp_des = 0.446579342e2
 
-        self.assertAlmostEqual(self.props2.get_properties("p")*1e-3, p_des, delta=1e-7)
-        self.assertAlmostEqual(self.props2.get_properties("h"), h_des, delta=1e-5)
-        self.assertAlmostEqual(self.props2.get_properties("u"), u_des, delta=1e-5)
-        self.assertAlmostEqual(self.props2.get_properties("s"), s_des, delta=1e-8)
-        self.assertAlmostEqual(self.props2.get_properties("cp"), cp_des, delta=1e-7)
+        self.assertAlmostEqual(self.props2.get_properties(rho=200, t=650, desc="p")*1e-3, p_des, delta=1e-7)
+        self.assertAlmostEqual(self.props2.get_properties(rho=200, t=650, desc="h"), h_des, delta=1e-5)
+        self.assertAlmostEqual(self.props2.get_properties(rho=200, t=650, desc="u"), u_des, delta=1e-5)
+        self.assertAlmostEqual(self.props2.get_properties(rho=200, t=650, desc="s"), s_des, delta=1e-8)
+        self.assertAlmostEqual(self.props2.get_properties(rho=200, t=650, desc="cp"), cp_des, delta=1e-7)
 
     def test_properties3(self):
         p_des = 0.783095639e2
@@ -42,11 +43,11 @@ class TestRegion3(unittest.TestCase):
         s_des = 0.446971906e1
         cp_des = 0.634165359e1
 
-        self.assertAlmostEqual(self.props3.get_properties("p")*1e-3, p_des, delta=1e-7)
-        self.assertAlmostEqual(self.props3.get_properties("h"), h_des, delta=1e-5)
-        self.assertAlmostEqual(self.props3.get_properties("u"), u_des, delta=1e-5)
-        self.assertAlmostEqual(self.props3.get_properties("s"), s_des, delta=1e-8)
-        self.assertAlmostEqual(self.props3.get_properties("cp"), cp_des, delta=1e-8)
+        self.assertAlmostEqual(self.props3.get_properties(rho=500, t=750, desc="p")*1e-3, p_des, delta=1e-7)
+        self.assertAlmostEqual(self.props3.get_properties(rho=500, t=750, desc="h"), h_des, delta=1e-5)
+        self.assertAlmostEqual(self.props3.get_properties(rho=500, t=750, desc="u"), u_des, delta=1e-5)
+        self.assertAlmostEqual(self.props3.get_properties(rho=500, t=750, desc="s"), s_des, delta=1e-8)
+        self.assertAlmostEqual(self.props3.get_properties(rho=500, t=750, desc="cp"), cp_des, delta=1e-8)
 
 
 if __name__ == "__main__":
