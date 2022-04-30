b2f82e2a352388d00efbc72728623c2d08015eb2
Noertri <trinuruns@gmail.com>
2022-04-30 08:22:53 +0700

    mengganti nama folder desktop_app menjadi desktop, mengubah nama modul menjadi gui.py

---
+++ desktop/gui.py
@@ -0,0 +1,17 @@
+from IF97 import if97, PRESSC, PRESST, TEMPC, TEMPT, BIGR
+import math
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+
+
+class PyWater(tk.Tk):
+
+    def __init__(self):
+
+        #Inisialisasi
+        super().__init__()
+        self.mainloop()
+
+
+_ui_ = PyWater()
